35863b777610a34d68c6a063bd51ff4bc049026d
Ewa Nizioł <kiepas.ewa@gmail.com>
2026-01-02 15:08:43 +0100

    change CIDR

--- Network/main.tf
+++ Network/main.tf
@@ -7,7 +7,7 @@ module "network" {
     source = "./modules/vpc"
     env = "dev"
     vpc_cidr = "10.0.0.0/16" 
-    public_subnet_cidr = [ "10.1.0.0/24", "10.2.0.0/24" ]
-    private_subnet_cidr = [ "10.0.10.0/24", "10.0.11.0/24" ]
+    public_subnet_cidr = [ "10.0.1.0/24", "10.0.2.0/24" ]
+    private_subnet_cidr = [ "10.0.10.0/24", "100.0.11.0/24" ]
   
 }
